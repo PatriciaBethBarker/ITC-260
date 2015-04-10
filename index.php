efb9dc707da483bcd9d7eb6d39ffc51a4b3050b2
Patricia Beth Barker <patriciabethbarker@gmail.com>
2015-04-09 17:03:35 -0700

    changes made with Curtis

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
+SESSION_start{};
+//this goes in front of every session 
 //the php must be the first code on the page or will not work
 //the passwords.php has to be first to allow the access
-include("passwords.php");
+//include("passwords.php");
 //params: server, username, password, database - gitignore keeps it secret
 //you want to keep things off your source code…so do not put them here
 // create passwords.php
@@ -11,48 +13,21 @@ include("passwords.php");
 
 //with key-value pairs 
 
-	if (isset($_SESSION["your_name"])) {
-    $name = $_SESSION["your_name"];
-}
-        if (isset($_REQUEST["name"])) {
-            setsession(
-            "your_name",
-            $_REQUEST["name"],
-            time() + 5 * 60,
-            "/",
-            no-ip.org
-);
-    $name = $_REQUEST["name"];
-}
+
+//session_register("password");
+
 
 //echo statements check the value of the double quoted name variable
 //if you leave off the method in the form it becomes a GET
 ?>
 <!DOCTYPE html>
 <html>
-        <form action="index.php">
-        <input
-            placeholder="First Name" name="name"
-            value="<?= $name ?>"
-        >
+<body>
+        <form method="post">
+        Password: <input placeholder="Password" name="password" type="text" />
         <input type="Submit" /></form>            
-    <pre>
-<?php
-        print_r($_SESSION);
-        echo time();
-?>
-    </pre>
-?>
 
 
-<?php
-
-
-//<form action="insert.php" method="post">
-//Name: <input type="text" name="name" />
-//Password: <input type="text" name="password" />
-//<input type="Submit" /></form>
-?>
 
 
 
--- passwords.php
+++ passwords.php
@@ -1,3 +1,14 @@
 <?php
-
+SESSION_start{};
+    $_SESSION ["password"] = "abc123";
+    if isset($_POST ["password"]){
+        //if what was entered has a value - is it equal to the session
+       if $_POST ["passsword"] == $_SESSION ["password"]{
+        //password is correct
+        echo "Correct Password"
+       } else{
+        //password is incorrect
+        echo "Wrong Password";
+       }
+    }
 ?>
