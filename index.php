ba3ce9523385d9c4a353058371e320487582e760
Patricia Beth Barker <patriciabethbarker@gmail.com>
2015-04-16 15:09:05 -0700

    index.php
    changed brackets to paren

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-SESSION_start{};
+SESSION_start();
 //this goes in front of every session 
 //the php must be the first code on the page or will not work
 //the passwords.php has to be first to allow the access
@@ -7,16 +7,10 @@ SESSION_start{};
 //params: server, username, password, database - gitignore keeps it secret
 //you want to keep things off your source code…so do not put them here
 // create passwords.php
-
 ?>
 <?php
-
 //with key-value pairs 
-
-
 //session_register("password");
-
-
 //echo statements check the value of the double quoted name variable
 //if you leave off the method in the form it becomes a GET
 ?>
@@ -33,4 +27,3 @@ SESSION_start{};
 
 
     </body>
-</html>
