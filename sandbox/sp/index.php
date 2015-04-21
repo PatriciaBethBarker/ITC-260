aa1da8cac4e094d9fb594ec92b8d9e102f003c1c
Patricia Beth Barker <patriciabethbarker@gmail.com>
2015-04-20 18:43:28 -0700

    track changes

---
+++ sandbox/sp/index.php
@@ -0,0 +1,31 @@
+<?php
+SESSION_start();
+//this goes in front of every session 
+//the php must be the first code on the page or will not work
+//the passwords.php has to be first to allow the access
+//include("passwords.php");
+//params: server, username, password, database - gitignore keeps it secret
+//you want to keep things off your source code…so do not put them here
+// create passwords.php
+?>
+<?php
+//with key-value pairs 
+//session_register("password");
+//echo statements check the value of the double quoted name variable
+//if you leave off the method in the form it becomes a GET
+?>
+<!DOCTYPE html>
+<html>
+<body>
+        <form method="post">
+        Password: <input placeholder="Password" name="password" type="text" />
+        <input type="Submit" /></form>            
+
+    <h1>Welcome!</h1>
+  <p>This is our flow chart <a href="http://www.tcbcommercialproperties.com/sandbox/sp/flowchart.png">A3: Session Passphrase</a>
+
+
+
+
+
+    </body>
